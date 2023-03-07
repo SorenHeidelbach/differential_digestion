f23bfae51938dc02a72e2e9724c0e276aa769eaf
SorenHeidelbach <lx38ll@bio.aau.dk>
2023-03-07 11:27:01 +0100

    Utility script for rendering Rmarkdown report

---
+++ scripts/render_reports.R
@@ -0,0 +1,12 @@
+setwd("/home/ubuntu/vol_store/differential_digestion")
+
+
+render_fun <- function(rmd) {
+  rmarkdown::render(
+    rmd,
+    output_dir = paste0("analysis/", basename(rmd), "/"),
+    clean = FALSE
+  )
+}
+
+render_fun("scripts/initial_investigation.Rmd")
